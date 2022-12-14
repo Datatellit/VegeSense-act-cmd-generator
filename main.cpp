381bd367577249fd1b6d779f3fa1a4ee095943a4
sunbaoshi <sunbaoshi1975@gmail.com>
2022-12-13 22:54:32 -0500

    More examples

--- main.cpp
+++ main.cpp
@@ -11,17 +11,19 @@ int main()
     // Step 1: 初始化，请输入参数
     // --> 要生成Act-cmd命令组的uid，需要与Action uid对应(1到126之间)
     // --> 要生成Act-cmd命令组包含的命令数量(1至16之间)
-    // --> 示例：uid = 1, cmdCount = 3
-    lv_actCmd.Init(1, 3);
+    // --> 示例：uid = 1, cmdCount = 4
+    lv_actCmd.Init(1, 4);
 
     //------------------------------------------------------------------
     // Step 2: 依次添加命令，总数就是上面初始化时输入的
     // --> 示例：第1条命令，快速打开风扇
     lv_actCmd.AddCmd_PowerSwitch(DevTag_Fan, DEVICE_SW_ON);
-    // --> 示例：第2条命令，场景切换到1
-    lv_actCmd.AddCmd_ChangeScenario(1);
+    // --> 示例：第2条命令，上条命令执行后再过10秒钟切换到场景1
+    lv_actCmd.AddCmd_ChangeScenario(1, 10);
     // --> 示例：第3条命令，照明亮度用60秒时间调整到100
     lv_actCmd.AddCmd_BRControl(100, 0, 0, 60);
+    // --> 示例：第4条命令，上条命令执行后再过15秒钟打开水泵
+    lv_actCmd.AddCmd_SetState(DevTag_Dummy + RelayKey_Pump, 1, 15);
     
     //------------------------------------------------------------------
     // Step 3: 生成消息
